e55319cfd8be93b2d8d6ac6cc5d74b5d58d74562
Jack Clackett <jack@noquarter.co>
2022-01-03 16:38:22 +0000

    fix: clean up forgot password

--- app/routes/__home/forgot-password.tsx
+++ app/routes/__home/forgot-password.tsx
@@ -1,27 +1,38 @@
+import * as React from "react"
 import * as c from "@chakra-ui/react"
-import { z } from "zod"
 import { ActionFunction, Form, Link, useActionData, useTransition } from "remix"
+import { z } from "zod"
+
 import { ActionData, validateFormData } from "~/lib/form"
+import { useToast } from "~/lib/hooks/useToast"
 import { badRequest } from "~/lib/remix"
 import { sendResetPasswordLink } from "~/services/auth/auth.service"
 
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData()
-  const resetSchema = z.object({
-    email: z.string().email("Invalid email"),
-  })
+  const resetSchema = z.object({ email: z.string().email("Invalid email") })
   const { data, fieldErrors } = await validateFormData(resetSchema, formData)
   if (fieldErrors) return badRequest({ fieldErrors, data })
   await sendResetPasswordLink(data)
-  return null
+  return true
 }
 
 type ResetInput = {
   email: string
 }
 export default function ForgotPassword() {
-  const { state } = useTransition()
+  const { state, type } = useTransition()
+  const inputRef = React.useRef<HTMLInputElement>(null)
   const action = useActionData<ActionData<ResetInput>>()
+  const toast = useToast()
+  React.useEffect(() => {
+    if (type === "actionSubmission") {
+      toast({ description: "Reset link sent to your email", status: "success" })
+      if (!inputRef.current) return
+      inputRef.current.value = ""
+    }
+  }, [type])
+
   return (
     <c.Center flexDir="column" pt={10}>
       <c.Box w={["100%", 400]}>
@@ -33,6 +44,7 @@ export default function ForgotPassword() {
             <c.FormControl isInvalid={!!action?.fieldErrors?.email}>
               <c.FormLabel htmlFor="email">Email address</c.FormLabel>
               <c.Input
+                ref={inputRef}
                 defaultValue={action?.data?.email}
                 id="email"
                 name="email"
